b73ba498882922bcaa8c4fe49cf99b916e044da2
Mauricio Mattinen <mauricio.jr.mattinen.valdez@gmail.com>
2024-01-31 08:05:22 -0400

    segundo commit cambio titulos

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Reconocimiento Voz',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 27, 0, 74)),
         useMaterial3: true,
       ),
       home: Speech(),
